41001be125760fffff36d91f730616fa20c768cd
Miguel Graciá <miguel.gracia@dreamsite.es>
2016-07-06 12:05:28 +0200

    Refactorizando el plugin de ckeditor para añadir tags de video html5. Ahora ya se muestra el botón "ver servidor" para la elección del archivo de video

--- public/administrator/js/classes/wysiwyg.js
+++ public/administrator/js/classes/wysiwyg.js
@@ -5,6 +5,35 @@ $(function () {
 
         this.init = function() {
             var applyCKEditorEvents = false;
+
+            var setBrowseButton = function (browseButton, event) {
+                var cleanUpFuncRef = CKEDITOR.tools.addFunction(function ()
+                {
+                    // Do the clean-up of filemanager here (called when an image was selected or cancel was clicked)
+                    $('#filemanager_iframe').remove();
+                    $("body").css("overflow-y", "scroll");
+                });
+
+                var editor = event.editor;
+
+                browseButton.hidden = false;
+                browseButton.onClick = function (dialog, i)
+                {
+                    editor._.filebrowserSe = this;
+                    var base_url = document.location.protocol + '//' + document.location.host + '/admin/media';
+                    var iframe = $("<iframe id='filemanager_iframe' class='fm-modal'/>").attr({
+                        src: base_url + // Change it to wherever  Filemanager is stored.
+                        '?modal=on&CKEditorFuncNum=' + CKEDITOR.instances[event.editor.name]._.filebrowserFn +
+                        '&CKEditorCleanUpFuncNum=' + cleanUpFuncRef +
+                        '&langCode=es' +
+                        '&CKEditor=' + event.editor.name
+                    });
+
+                    $("body")
+                        .css("overflow-y", "hidden") // Get rid of possible scrollbars in containing document
+                        .append(iframe);
+                }
+            };
             for(var idx = 0; idx < textarea.length; idx++) {
 
                 if(!textarea[idx].classList.contains('no-editor')) {
@@ -14,47 +43,49 @@ $(function () {
             }
 
             if(applyCKEditorEvents) {
+
                 CKEDITOR.on('dialogDefinition', function (event)
                 {
-                    var editor = event.editor;
                     var dialogDefinition = event.data.definition;
                     var dialogName = event.data.name;
-
-                    var cleanUpFuncRef = CKEDITOR.tools.addFunction(function ()
-                    {
-                        // Do the clean-up of filemanager here (called when an image was selected or cancel was clicked)
-                        $('#filemanager_iframe').remove();
-                        $("body").css("overflow-y", "scroll");
-                    });
 
                     var tabCount = dialogDefinition.contents.length;
 
                     for (var i = 0; i < tabCount; i++) {
-
                         var browseButton;
 
-                        if(typeof dialogDefinition.contents[i] != 'undefined') {
-                            browseButton = dialogDefinition.contents[i].get('browse')
-                        }
+                        if(dialogName == 'video') {
+                            var elements = dialogDefinition.contents[i].elements;
+                            var elementsCount = elements.length;
 
-                        if (browseButton !== null) {
-                            browseButton.hidden = false;
-                            browseButton.onClick = function (dialog, i)
-                            {
-                                editor._.filebrowserSe = this;
-                                var iframe = $("<iframe id='filemanager_iframe' class='fm-modal'/>").attr({
-                                    src: 'http://abs.local/admin/media' + // Change it to wherever  Filemanager is stored.
-                                    '?modal=on&CKEditorFuncNum=' + CKEDITOR.instances[event.editor.name]._.filebrowserFn +
-                                    '&CKEditorCleanUpFuncNum=' + cleanUpFuncRef +
-                                    '&langCode=es' +
-                                    '&CKEditor=' + event.editor.name
-                                });
+                            if(typeof dialogDefinition.contents[i] != 'undefined') {
+                                for(var e = 0; e < elementsCount; e++) {
+                                    var hboxElement = dialogDefinition.contents[i].elements[e];
+                                    if(typeof hboxElement.children != 'undefined') {
+                                        var hboxChildrenCount = hboxElement.children.length;
+                                        for(var h = 0; h < hboxChildrenCount; h++) {
+                                            if(typeof hboxElement.children[h].filebrowser != 'undefined') {
+                                                browseButton = hboxElement.children[h];
+                                                setBrowseButton(browseButton, event);
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        } else {
 
-                                $("body").append(iframe);
-                                $("body").css("overflow-y", "hidden");  // Get rid of possible scrollbars in containing document
+                            if(typeof dialogDefinition.contents[i] != 'undefined') {
+                                browseButton = dialogDefinition.contents[i].get('browse');
+
+                                if (browseButton !== null) {
+                                    setBrowseButton(browseButton, event);
+                                }
                             }
                         }
                     }
+
+
+
                 }); // dialogDefinition*/
             }
         };
