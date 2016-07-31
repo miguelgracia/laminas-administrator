a5d063a86041dfba8531d4b9c487603b1ccce4af
Miguel Graciá <miguelgraciamartin@gmail.com>
2016-07-31 02:59:33 +0200

    Animacion de la HOME

--- public/absconsultor/js/controllers/srmHomeController.js
+++ public/absconsultor/js/controllers/srmHomeController.js
@@ -4,6 +4,25 @@ function srmHomeController() {
 
     this.index = function() {
 
+        var $nav = $('nav');
+
+        $nav.css('top','-100%');
+
+        var $homeBrandLogo = $('#home_brand_logo');
+        var $homeNav = $(".video-wrapper .nav");
+
+        $(window).scroll(function (e) {
+            if($(this).scrollTop() < 50) {
+                $homeBrandLogo.removeClass('header');
+                $homeNav.removeClass('header');
+
+                $nav.css('top','-' + ($nav.height()) + 'px');
+            } else {
+                $homeBrandLogo.addClass('header');
+                $homeNav.addClass('header');
+                $nav.css('top','0');
+            }
+        });
         /*$('.bxslider').bxSlider({
             mode: 'fade',
             video: true,
