1243f211a2bfa9dd28959ebf8d28a6b1a5744d2b
Miguel Graciá <miguelgraciamartin@gmail.com>
2019-11-16 18:06:54 +0100

    Remove unused function in AmHomeModuleController.php

--- data/module/AmHome/src/AmHome/Controller/AmHomeModuleController.php
+++ data/module/AmHome/src/AmHome/Controller/AmHomeModuleController.php
@@ -5,15 +5,4 @@ namespace AmHome\Controller;
 use Administrator\Controller\AuthController;
 
 class AmHomeModuleController extends AuthController
-{
-    protected $userTable;
-    public function setControllerVars()
-    {
-        $this->userTable = $this->serviceLocator->get('AmUser\Model\UserTable');
-    }
-
-    public function indexAction()
-    {
-
-    }
-}
+{}
