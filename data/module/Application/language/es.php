c7e8b1309eeb16d942ccf87b1b438bc86a27dd21
Miguel Graciá <miguelgraciamartin@gmail.com>
2020-01-07 17:17:41 +0100

    (feat) Traduccion mensaje cookies

--- data/module/Application/language/es.php
+++ data/module/Application/language/es.php
@@ -2,7 +2,8 @@
 
 return [
     'Our Partners' => 'Nuestros colaboradores',
-    "I agree with the terms and conditions of use"
-    => "He leído y acepto los <a href='%s' target='_blank'>términos y condiciones de uso</a>"
-
+    "I agree with the terms and conditions of use" => "He leído y acepto los <a href='%s' target='_blank'>términos y condiciones de uso</a>",
+    "Cookie message" => "Esta Web utiliza cookies analíticas de terceros para facilitar la navegación y obtener información de uso de sus visitantes. Si continúa navegando, consideraremos que acepta su uso. Puede obtener más información en nuestra política de cookies.",
+    "More info" => "+ Información",
+    "Cookies agree" => "De acuerdo",
 ];
